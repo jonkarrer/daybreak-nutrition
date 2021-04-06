fd642247c508c5b050b9acd44f540453248f93c8
jonkarrer <karrerjon@gmail.com>
2021-04-05 19:37:18 -0500

    added backdrop

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const hamburgTopLine = document.querySelector(".top-line");
-const hamburgMidLine = document.querySelector(".mid-line");
-const hamburgBottLine = document.querySelector(".bott-line");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 let isOpen = false;
-const openMenu = () => {
+
+hamburgerIcon.addEventListener("click", openMenu);
+function openMenu() {
+  const hamburgTopLine = document.querySelector(".top-line");
+  const hamburgMidLine = document.querySelector(".mid-line");
+  const hamburgBottLine = document.querySelector(".bott-line");
   if (!isOpen) {
     hamburgTopLine.style.transform = `rotate(330deg)`;
     hamburgBottLine.style.transform = `rotate(330deg)`;
     hamburgMidLine.style.display = "none";
     isOpen = true;
+    controlFrontLayer("33vh");
     return;
   }
   hamburgTopLine.style.transform = `rotate(0deg)`;
   hamburgMidLine.style.display = "block";
   hamburgBottLine.style.transform = `rotate(0deg)`;
   isOpen = false;
-};
-hamburgerIcon.addEventListener("click", openMenu);
+  controlFrontLayer("65px");
+}
+function controlFrontLayer(distanceFromTop) {
+  const frontLayer = document.getElementById("front-layer");
+  frontLayer.style.top = `${distanceFromTop}`;
+}
