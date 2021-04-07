29e82d2d4c030aa823f5a519f7d77b2b6ff32909
jonkarrer <karrerjon@gmail.com>
2021-04-06 22:41:51 -0500

    updated button links

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 let isOpen = false;
-
+//Hamburgur icon animation
 hamburgerIcon.addEventListener("click", openMenu);
 function openMenu() {
   const hamburgTopLine = document.querySelector(".top-line");
@@ -11,16 +11,32 @@ function openMenu() {
     hamburgBottLine.style.transform = `rotate(330deg)`;
     hamburgMidLine.style.display = "none";
     isOpen = true;
-    controlFrontLayer("33vh");
+    //Drop front layer for basic menu
+    controlFrontLayer("18vh");
     return;
   }
   hamburgTopLine.style.transform = `rotate(0deg)`;
   hamburgMidLine.style.display = "block";
   hamburgBottLine.style.transform = `rotate(0deg)`;
   isOpen = false;
-  controlFrontLayer("65px");
+  //Set default dropdown position
+  controlFrontLayer("60px");
 }
 function controlFrontLayer(distanceFromTop) {
   const frontLayer = document.getElementById("front-layer");
   frontLayer.style.top = `${distanceFromTop}`;
+}
+//Navigation buttons
+let nav = document.getElementById("nav-links");
+let allLinks = nav.children;
+const changeBackColor = (evt) => {
+  for (link of allLinks) {
+    link.style.color = "black";
+    link.style.background = "rgba(0, 0, 0, 0.06)";
+  }
+  evt.target.style.background = "black";
+  evt.target.style.color = "white";
+};
+for (link of allLinks) {
+  link.addEventListener("click", changeBackColor, false);
 }
