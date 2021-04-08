104e6f12bb9975ff1f7f81e3c71bc2dd7300d67c
jonkarrer <karrerjon@gmail.com>
2021-04-08 16:44:47 -0500

    added slideshow

--- index.js
+++ index.js
@@ -44,3 +44,25 @@ const changeBackColor = (evt) => {
 for (link of allLinks) {
   link.addEventListener("click", changeBackColor, false);
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
