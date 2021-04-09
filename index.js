610d6fa8c302cc8cf07712183273ce0dd80dee8b
jonkarrer <karrerjon@gmail.com>
2021-04-08 19:11:14 -0500

    navbar edit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class HamburgerIcon {
     this.bottLine.style.transform = `rotate(0deg)`;
     this.open = false;
     //Set default dropdown position
-    this.controlFrontLayer("8vh");
+    this.controlFrontLayer("9vh");
   };
   controlFrontLayer(distanceFromTop) {
     const frontLayer = document.getElementById("front-layer");
